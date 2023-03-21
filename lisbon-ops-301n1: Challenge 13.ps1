daa1966520eaff031a0c67014321691aa674d854
Sérgio Charruadas <sergiomiguel450@gmail.com>
2023-03-21 08:02:39 +0000

    init

---
+++ lisbon-ops-301n1: Challenge 13.ps1
@@ -0,0 +1,27 @@
+# Connect to Active Directory
+Import-Module ActiveDirectory
+
+# Create new user
+New-ADUser -Name "Franz Ferdinand" `
+    -GivenName "Franz" `
+    -Surname "Ferdinand" `
+    -SamAccountName "FranzFerdinand" `
+    -Path "CN=Users,DC=corp,DC=globexpower,DC=com" `
+    -AccountPassword "P@ssw0rd1" `
+    -Title "TPS Reporting Lead" `
+    -Department "TPS Department" `
+    -Company "GlobeX USA" `
+    -EmailAddress "ferdi@globexpower.com" `
+    -City "Springfield, OR"
+
+# Create new group
+New-ADGroup -Name "TPS Department" `
+    -Path "OU=GlobeX USA,DC=corp,DC=globexpower,DC=com" `
+    -GroupScope Global `
+    -GroupCategory Security `
+    -Description "Group for TPS Department"
+
+# Create new OU
+New-ADOrganizationalUnit -Name "GlobeX USA" `
+    -Path "OU=Company,DC=corp,DC=globexpower,DC=com"
+
